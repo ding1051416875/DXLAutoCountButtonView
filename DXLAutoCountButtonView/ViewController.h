a0f67fe469d030863a5ae25cf8787e74326186ae
安阳发往南乐 <dingxiaolei@dingxiaoleidemacbook-pro.local>
2017-11-24 09:26:42 +0800

    Initial Commit

---
+++ DXLAutoCountButtonView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DXLAutoCountButtonView
+//
+//  Created by 丁晓雷 on 2017/11/24.
+//  Copyright © 2017年 丁晓雷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DXLAutoCountButtonView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DXLAutoCountButtonView
+//
+//  Created by 丁晓雷 on 2017/11/24.
+//  Copyright © 2017年 丁晓雷. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
